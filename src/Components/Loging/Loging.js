9485ada1c8287eca1d28b0594b4a843fa93060a9
saikulislam4 <saikulislam505@gmail.com>
2023-03-22 02:54:17 +0600

    modified

--- src/Components/Loging/Loging.js
+++ src/Components/Loging/Loging.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUserTie} from 'react-icons/fa';
-import { RiLockPasswordLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
-
-
-
-
 const Loging = () => {
+
+    const [emailError, setEmailError]= useState('')
+    const [passwordError, setpasswordError]= useState('')
+
+// --------------Email handel function--------------
+const [email, setEmail]= useState('')
+
+const handlelSubmit = (e) =>{
+    e.preventDefault()
+    if(!email && setEmail){setEmailError('Email is Require')}
+    else if(!password && setPassword){setpasswordError('Password is Require')}
+ 
+}
+const handelEmail = (e) =>{
+    setEmail(e.target.value)
+    setEmailError('')
+}
+// --------------Email handel function--------------
+const [password, setPassword]= useState('')
+
+const handelPassword = (e) =>{
+    setPassword(e.target.valu)
+    setpasswordError('')
+}
+
+
     return (
         <div className=' flex justify-center items-center container mx-auto w-40% m-auto text h-screen '>
             <div className="loging border-spacing-1 px-20 py-10 bg-black bg-opacity-20 rounded-lg  text-center">
@@ -16,14 +37,16 @@ const Loging = () => {
 
                 </div>
                 <h1 className=' text-5xl'>User logIn</h1>
+                <form onSubmit={handlelSubmit}>
                 <div className="text">
                     
-                    <div className="email my-10">
-                    <input className='p-2 bg-transparent border rounded-sm outline-0 outline-offset-0'  type="text"  placeholder='type your email/number'/>
+                    <div className="email my-10 relative">
+                    <input onChange={handelEmail} className='p-2 bg-transparent border rounded-sm outline-0 outline-offset-0'  type="text"  placeholder='type your email/number'/>
+                    <h1 className='text-red-700 text-start mt-1 absolute mt text-[18px]'>{emailError}</h1>
                     </div>
-                    <div className="email flex">
-                    <input className='p-2 bg-transparent border rounded-sm' type="password"  placeholder='type your password'/> 
-                    {/* <RiLockPasswordLine></RiLockPasswordLine> */}
+                    <div className="email flex relative">
+                    <input onChange={handelPassword} className='p-2 bg-transparent border rounded-sm' type="password"  placeholder='type your password'/> 
+                    <h1 className='text-red-700 text-start mt-14 absolute mt text-[18px]'>{passwordError}</h1>
                     </div>
                    
                 </div>
@@ -35,11 +58,12 @@ const Loging = () => {
                  
                 </div>
                 <div className="button ">
-                <Link to="/"className=" block font-bold px-12 mb-3 py-3 text-bg-white rounded-lg text-black hover:bg- bg-white hover:text-white hover:bg-red-600 ">Login</Link>
-                <Link to="/"className=" block font-bold px-12 py-3 text-bg-white rounded-lg text-black hover:bg- bg-white hover:text-white hover:bg-red-600">create account</Link>
+                <button className=' w-full block font-bold px-12 mb-3 py-3 text-bg-white rounded-lg text-black hover:bg- bg-white hover:text-white hover:bg-red-600 ' type='submit'>Login</button>
+                <Link className=" block font-bold px-12 py-3 text-bg-white rounded-lg text-black hover:bg- bg-white hover:text-white hover:bg-red-600">create account</Link>
+                <button type='submit'>test</button>
                 </div>
                 
-            
+                </form>
             </div>
                 
         </div>
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -6,6 +6,7 @@ const Registration = () => {
     return (
         <div>
             <div className=' flex justify-center items-center container mx-auto w-40% m-auto text h-screen '>
+                    <form action="">
             <div className=" loging border-spacing-1 px-20 py-10 bg-black bg-opacity-20 rounded-lg  text-center">
                 <div className="icon  text-9xl justify-center flex mb-3 ">
                     <FaUserTie></FaUserTie>
@@ -13,7 +14,6 @@ const Registration = () => {
                 </div>
                 <h1 className=' text-5xl mb-10'>Create an Account</h1>
                 <div className="text grid grid-cols-1">
-                    
                     <div className="email  mb-5 grid grid-cols-2">
                     <label className=' w-40 text-end' htmlFor="First Name"> Full Name : </label>
                     <input className=' p-2 bg-transparent border rounded-sm outline-0 outline-offset-0'  type="text"  placeholder='Type Your Full Name'/>
@@ -37,9 +37,11 @@ const Registration = () => {
                     <input type="checkbox" name='Remember' className=' mr-2' />Remember
                     </div>
                 </div>
-                <Link to="/"className=" block font-bold px-12 py-3 text-bg-white rounded-lg text-black hover:bg- bg-white hover:text-white hover:bg-red-600">create account</Link>
+                <Link className=" block font-bold px-12 py-3 text-bg-white rounded-lg text-black hover:bg- bg-white hover:text-white hover:bg-red-600"><button type='submit'>create account</button></Link>
             
             </div>
+            </form>
+        
                 
         </div>
         </div>
